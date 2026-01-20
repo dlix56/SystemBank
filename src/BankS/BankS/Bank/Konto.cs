dc82cec94748387243cefea3550c907ce12d1959
dlix56 <agadula@student.agh.edu.pl>
2026-01-20 22:30:59 +0100

    Add Konto

---
+++ src/BankS/BankS/Bank/Konto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankS.Bank
+{
+    public abstract class Konto
+    {
+        string nrkonta;
+        decimal saldo;
+
+        public string NrKonta
+        {
+            get => nrkonta; set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Nr konta nie może być pusty");
+                nrkonta = value;
+            }
+        }
+
+        public decimal Saldo
+        {
+            get => saldo;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Saldo nie może być ujemne");
+                saldo = value;
+            }
+        }
+
+        protected Konto(string nrkonta, decimal saldoStart)
+        {
+            NrKonta = nrkonta;
+            Saldo = saldoStart;
+        }
+
+        public virtual void Wplac(decimal kwota)
+        {
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota musi byc dodatnia");
+
+            Saldo += kwota;
+        }
+
+        public virtual void Wyplac(decimal kwota)
+        {
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota musi być dodatnia");
+
+            if (kwota > Saldo)
+                throw new ArgumentException("Brak środków");
+
+            Saldo -= kwota;
+        }
+
+    }
+}
