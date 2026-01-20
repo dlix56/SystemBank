e20c6b04b094e755c0becca9a45792a3494447cb
dlix56 <agadula@student.agh.edu.pl>
2026-01-20 23:42:59 +0100

    Klasa KontoOsz

---
+++ src/BankS/BankS/Bank/KontoOszczednosc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankS.Bank
+{
+    public class KontoOszczednosc : Konto
+    {
+        decimal procent;
+
+        public decimal Procent { get; set; }
+    
+        public KontoOszczednosc(string nrkonta, decimal procent, decimal saldoStart)
+            : base(nrkonta, saldoStart)
+        {
+            if (procent < 0)
+            {
+                throw new ArgumentException("Oprocentowanie nie jest ujemne");
+            }
+            this.procent = procent;
+        }
+
+        public override void Wyswietl()
+        {
+            Console.WriteLine($"Konto oszczędnościowe: {NrKonta}, saldo: {Saldo} zł, oprocentowanie: {Procent}%");
+        }
+    }
+}
